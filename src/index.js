b055ca21e238b3b8b88a6a4454c5795bbba5648f
Brandon Blair <brandon.d.blair@me.com>
2017-04-08 22:25:53 -0600

    Setting up basic routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
 
-import StorePicker from './components/StorePicker';
 import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App/>, document.querySelector('#main'));
+const Route = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeID" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+render(<Route/>, document.querySelector('#main'));
