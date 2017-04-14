b0bba696acb01b6643c342502213a118ff4e7365
Brandon Blair <brandon.d.blair@me.com>
2017-04-14 09:45:52 -0600

    ignoring base.js

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyCzdhoMZYaLYY9VKq4ljsRJbLvuWw_05j8',
+  authDomain: 'catch-of-the-day-blair.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-blair.firebaseio.com'
+});
+
+export default base;
